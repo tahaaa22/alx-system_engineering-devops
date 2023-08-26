12ea53218d3a99346e175f8a0feda3ab7d9121b4
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-08-25 17:00:23 -0700

    testing for strtok

--- str.c
+++ str.c
@@ -1,4 +1,47 @@
+#include <stdio.h>
+#include <string.h>
 
+/**
+ * find_token_start - Find the start of the next token.
+ * @buffer: The buffer to search.
+ * @delim: The delimiter characters.
+ *
+ * Return: A pointer to the start of the next token.
+ */
+char *find_token_start(char *buffer, const char *delim)
+{
+	while (*buffer)
+	{
+		if (!strchr(delim, *buffer))
+			break;
+		buffer++;
+	}
+
+	return (buffer);
+}
+
+/**
+ * find_token_end - Find the end of the current token.
+ * @token_start: The start of the current token.
+ * @delim: The delimiter characters.
+ *
+ * Return: A pointer to the end of the current token.
+ */
+char *find_token_end(char *token_start, const char *delim)
+{
+	char *token_end = token_start + 1;
+
+	while (*token_end)
+	{
+		if (strchr(delim, *token_end))
+			break;
+		token_end++;
+	}
+
+	return (token_end);
+}
+
+/**
  * my_strtok - Tokenize a string.
  * @str: The string to be tokenized.
  * @delim: The delimiter characters.
@@ -9,40 +52,23 @@ char *my_strtok(char *str, const char *delim)
 {
 	static char *buffer;
 
-	if (str != NULL)
+	if (str)
 		buffer = str;
 
-	if (buffer == NULL || *buffer == '\0')
+	if (!buffer || !(*buffer))
 		return (NULL);
 
-	/* Find the start of the next token */
-	char *token_start = buffer;
+	char *token_start = find_token_start(buffer, delim);
 
-	while (*token_start != '\0')
-	{
-		if (strchr(delim, *token_start) == NULL)
-			break;
-		token_start++;
-	}
-
-	if (*token_start == '\0')
+	if (!(*token_start))
 	{
 		buffer = NULL;
 		return (NULL);
 	}
 
-	/* Find the end of the token */
-	char *token_end = token_start + 1;
+	char *token_end = find_token_end(token_start, delim);
 
-	while (*token_end != '\0')
-	{
-		if (strchr(delim, *token_end) != NULL)
-			break;
-		token_end++;
-	}
-
-	/* Null-terminate the token and update the buffer */
-	if (*token_end != '\0')
+	if (*token_end)
 	{
 		*token_end = '\0';
 		buffer = token_end + 1;
